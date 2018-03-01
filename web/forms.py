c6b3b6390fcb5dccdca408a270c2ecf651c78534
lekro <kapuraya@gmail.com>
2018-03-01 17:32:07 -0600

    Add integral field type

--- web/forms.py
+++ web/forms.py
@@ -29,13 +29,24 @@ def make_form(config):
             formfield = StringField(**kwargs)
         if field['type'] == 'area':
             formfield = TextAreaField(**kwargs)
+        if field['type'] == 'integer':
+            formfield = IntegerField(**kwargs)
+
         setattr(F, field['name'], formfield)
         dynamic_fields.append(field['name'])
+
+        # Add maskable checkbox
+        if 'maskable' in field and field['maskable']:
+            formfield = BooleanField(config['mask-label'])
+            setattr(f, field['name'] + '_mask', formfield)
+            dynamic_fields.append(field['name'] + config['mask-label'])
 
     form = F()
 
     for field in form:
         if field.name in dynamic_fields:
             field.flags.dynamic = True
+        if '_mask' in field.name:
+            field.flags.mask = True
     return form
 
