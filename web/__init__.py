802041db86615c93cf79ead95fd3751dabd2a7d2
lekro <kapuraya@gmail.com>
2018-02-28 18:43:24 -0600

    Load flask config

--- web/__init__.py
+++ web/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from ruamel.yaml import YAML
 
 app = Flask(__name__)
+yaml = YAML()
+
+with open('config.yml') as f:
+    flask_config = yaml.load(f)['flask']
+    app.config.update(**flask_config)
 
 from web import routes
